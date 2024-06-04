f7b1e63f34fabfb919affded3688772fed72d254
Danomolina <danomolina@gmail.com>
2024-06-04 21:16:04 +0000

    proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function getRandomElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+  function generateExcuse() {
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    who = getRandomElement(who);
+    action = getRandomElement(action);
+    what = getRandomElement(what);
+    when = getRandomElement(when);
+
+    return `${who} ${action} ${action} ${when}.`;
+  }
+
+  document.getElementById("excuse").innerText = generateExcuse();
 };
